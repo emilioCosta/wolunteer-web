59b8016cc87de00107ea8b80841a7217507500a7
Emilio Costa <emilio@MacBook-Pro-de-Emilio.local>
2021-09-07 11:51:03 -0300

    See CI effects

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Reddit <code>src/App.js</code> and save to reload.
+          Furtadinho <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
